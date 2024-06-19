2665d8f9e665566ff043db09cb2f901da248beeb
Il tuo nome <la_tua_email@example.com>
2024-06-19 17:43:22 +0200

    aggiunta sezione banner (da aggiustare) e sezione sponsors (con responsivness)

--- src/components/AppLuca.vue
+++ src/components/AppLuca.vue
@@ -12,17 +12,49 @@ return {
 
 <section class="buy-avada ">
     
-<div class="contenuto-banner d-flex  justify-content-center align-items-center flex-column">
-    <h2>"I've failed over and over again in my life. And that is why I succeded."</h2>
-    <p>icona</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate corporis sit repellat commodi. Excepturi quas provident dolorum ipsam, a ad sapiente mollitia, ipsum ducimus velit ex suscipit dignissimos ratione sequi.</p>
-    <button>bla</button>
-</div>
-
+  <div class="contenuto-banner d-flex  justify-content-center align-items-center flex-column">
+      <h2 class="">"I've failed over and over again in my life. And that is why I succeded."</h2>
+      <h3>i</h3>
+      <p>Arcu auctor gravida nisl, congue sit nisi tincidunt eget proin. In lacinia lacus donec sed massa in ipsum eros, tristique. Gravida suspendisse etiam in iaculis</p>
+      <input class="btn btn-primary" type="button" value="Buy Avada Today >">
+  </div>
 
 </section>
 
+<section class="sponsors py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-12 text-center mb-4">
+        <img src="../images/sponsor-1-2x-200x103.png" alt="Yoga Studio Logo" class="img-fluid mb-3">
+        <p>i</p>
+        <p>Tristique aliquam in nullam habitasse nunc, amet vel consectetur. Nulla in suspendisse dolor, aliquet.</p>
+        <a href="#">Visit Yoga Studio ></a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-12 text-center mb-4">
+        <img src="../images/sponsor-2-2x-200x103.png" alt="Fitness Center Logo" class="img-fluid mb-3">
+        <p>i</p>
+        <p>Feugiat etiam dui mauris pharetra mauris sed pharetra. Convallis sapien ornare cras faucibus nulla porta dui a.</p>
+        <a href="#">Join Fitness Center ></a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-12 text-center mb-4">
+        <img src="../images/sponsor-3-2x-200x103.png" alt="Fitness Club Logo" class="img-fluid mb-3">
+        <p>i</p>
+        <p>Nisl at quam adipiscing montes, nec, adipiscing commodo aliquam tincidunt. Ligula in habitant et sagittis imperdiet.</p>
+        <a href="#">Learn about FC ></a>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-12 text-center mb-4">
+        <img src="../images/sponsor-4-2x-200x103.png" alt="Power Gym Logo" class="img-fluid mb-3">
+        <p>i</p>
+        <p>Proin in viverra maecenas et mattis ut nibh enim. Gravida ultricies mi porta lacus. Et posuere velit sed nec eleifend.</p>
+        <a href="#">Visit Power Gym ></a>
+      </div>
+    </div>
+  </div>
+</section>
 
+<div>
+    <p>lorem*3</p>
+</div>
 
 </template>
 
@@ -32,13 +64,45 @@ return {
 
 .buy-avada{
     background-color: red;
-    height: 400px;
+    height: 700px;
     background-image: url(../../src/images/banner2-2x.jpg);
     background-size: cover; 
     background-position: center; 
-  background-repeat: no-repeat; 
+    background-repeat: no-repeat;
+    color: white;
+
+    .contenuto-banner{
+      margin: 0 auto;
+      text-align: center;
+      padding: 10rem 25rem;
+      background: rgba(0, 0, 0, 0.5);
+      height: 100%;
+
+      h2{
+        font-size: 3rem;
+        margin-bottom: 2rem;
+      }
+
+      p {
+        color: rgba(214, 213, 216, 0.75);
+        font-size: 1rem;
+        margin-bottom: 2rem;
+      }
+    }
 }
 
-
+.sponsors {
+  
+  p {
+    color: #666;
+    font-size: 1rem;
+  }
+  
+  a {
+    color: black;
+    text-decoration: none;
+   
+  }
+}
 
 </style>
