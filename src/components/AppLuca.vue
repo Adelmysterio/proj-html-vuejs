c42466212aa63c3a1de6442431e66e52aecc49db
Il tuo nome <la_tua_email@example.com>
2024-06-19 16:05:16 +0200

    inizio struttura di base banner

--- src/components/AppLuca.vue
+++ src/components/AppLuca.vue
@@ -10,10 +10,35 @@ return {
 
 <template>
 
+<section class="buy-avada ">
+    
+<div class="contenuto-banner d-flex  justify-content-center align-items-center flex-column">
+    <h2>"I've failed over and over again in my life. And that is why I succeded."</h2>
+    <p>icona</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate corporis sit repellat commodi. Excepturi quas provident dolorum ipsam, a ad sapiente mollitia, ipsum ducimus velit ex suscipit dignissimos ratione sequi.</p>
+    <button>bla</button>
+</div>
+
+
+</section>
+
+
+
 </template>
 
-<style lang=
-scss
- scoped>
+<style lang="scss" scoped>
+
+@use '../style/partials/_variables.scss' as *;
+
+.buy-avada{
+    background-color: red;
+    height: 400px;
+    background-image: url(../../src/images/banner2-2x.jpg);
+    background-size: cover; 
+    background-position: center; 
+  background-repeat: no-repeat; 
+}
+
+
 
 </style>
