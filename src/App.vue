eca770292c5866f1f8e81a58d618e25928361efb
AdelMysterio <storliniadelmo@gmail.com>
2024-06-19 11:46:11 +0200

    creato scaffolding

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  
+  components: {
+    AppHeader
+  },
+
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+<AppHeader />
+</template>
+
+<style lang="scss">
+@use './style/general.scss' as *;
+@use './style/partials/variables.scss' as *;
+@use './style/partials/mixins.scss' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+<header>
+    <h1 class="fs-5">Head</h1>
+    <i class="fa-solid fa-star"></i>
+</header>
+</template>
+
+<style lang=scss scoped></style>
