4c149e53970de0cf439a3709937a5d3be60a8800
AdelMysterio <storliniadelmo@gmail.com>
2024-06-20 09:56:09 +0200

    modificato html singlecard

--- src/components/ade_section/SingleCard.vue
+++ src/components/ade_section/SingleCard.vue
@@ -18,13 +18,13 @@ export default {
 </script>
 
 <template>
-    <div class="single-card" :style="{ backgroundImage: 'url(' + img + ')' }">
-        <div class="content-overlay">
+    <article class="single-card" :style="{ backgroundImage: 'url(' + img + ')' }">
+        <section class="content-overlay">
             <i class="fa-solid fa-stairs fa-rotate-by" style="--fa-rotate-angle: 45deg;"></i>
             <h2>{{ title }}</h2>
             <p>{{ description }}</p>
-        </div>
-    </div>
+        </section>
+    </article>
 </template>
 
 <style lang="scss" scoped>
